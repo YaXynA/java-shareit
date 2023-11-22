5426a15ad9e6f8619d3c5e69b7156212096581db
XynA <YaXymma@yandex.ru>
2023-11-22 10:58:57 +0300

    Исправил создание на возврат

--- src/main/java/ru/practicum/shareit/user/UserMapper.java
+++ src/main/java/ru/practicum/shareit/user/UserMapper.java
@@ -2,26 +2,23 @@ package ru.practicum.shareit.user;
 
 import org.springframework.stereotype.Component;
 import ru.practicum.shareit.user.dto.UserDto;
-import ru.practicum.shareit.user.User;
 
 @Component
 public class UserMapper {
 
     public UserDto returnUserDto(User user) {
-        UserDto userDto = UserDto.builder()
+        return UserDto.builder()
                 .id(user.getId())
                 .email(user.getEmail())
                 .name(user.getName())
                 .build();
-        return userDto;
     }
 
     public User returnUser(UserDto userDto) {
-        User user = User.builder()
+        return User.builder()
                 .id(userDto.getId())
                 .email(userDto.getEmail())
                 .name(userDto.getName())
                 .build();
-        return user;
     }
 }
