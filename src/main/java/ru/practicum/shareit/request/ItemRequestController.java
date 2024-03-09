2dbb6ccb620b3b73f7f90b217e3d9e36bd0ebbea
XynA <YaXymma@yandex.ru>
2024-03-09 06:20:34 +0300

    ТЗ 15 ревлизация

--- src/main/java/ru/practicum/shareit/request/ItemRequestController.java
+++ src/main/java/ru/practicum/shareit/request/ItemRequestController.java
@@ -1,12 +1,48 @@
 package ru.practicum.shareit.request;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.constants.Constants;
+import ru.practicum.shareit.request.dto.ItemRequestDto;
+import ru.practicum.shareit.request.dto.ItemRequestDtoOut;
+import ru.practicum.shareit.request.service.ItemRequestService;
 
-/**
- * TODO Sprint add-item-requests.
- */
+import javax.validation.Valid;
+import javax.validation.constraints.Min;
+import java.util.List;
+
 @RestController
 @RequestMapping(path = "/requests")
+@RequiredArgsConstructor
+@Slf4j
+@Validated
 public class ItemRequestController {
+
+    private final ItemRequestService requestService;
+
+    @PostMapping
+    public ItemRequestDtoOut add(@RequestHeader(Constants.USER_HEADER) Long userId,
+                                 @Valid @RequestBody ItemRequestDto requestDto) {
+        return requestService.add(userId, requestDto);
+    }
+
+    @GetMapping
+    public List<ItemRequestDtoOut> getUserRequests(@RequestHeader(Constants.USER_HEADER) Long userId) {
+        return requestService.getUserRequests(userId);
+    }
+
+    @GetMapping("/all")
+    public List<ItemRequestDtoOut> getAllRequests(@RequestHeader(Constants.USER_HEADER) Long userId,
+                                                  @RequestParam(name = "from", defaultValue = "0") @Min(0) Integer from,
+                                                  @RequestParam(value = "size", defaultValue = "10") @Min(1) Integer size) {
+        return requestService.getAllRequests(userId, from, size);
+    }
+
+    @GetMapping("/{requestId}")
+    public ItemRequestDtoOut get(@RequestHeader(Constants.USER_HEADER) Long userId,
+                                 @PathVariable Long requestId) {
+        return requestService.getRequestById(userId, requestId);
+    }
 }
